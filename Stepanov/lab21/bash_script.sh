b713f5161f8ed41a59155365267f652468a2a111
Alexstepano <113289408+Alexstepano@users.noreply.github.com>
2023-02-28 13:28:21 +0300

    Update bash_script.sh

--- Stepanov/lab21/bash_script.sh
+++ Stepanov/lab21/bash_script.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]
+if [[ $# -ne 2 ]]
    then
-	echo "Pleace , insert exactly 1 argument -size"
-        echo ". [scriptname].sh [size]"
-elif [[ $1 = '?' ]]
-   then
+	echo "Pleace , insert exactly argument with flag -s or --size: -s [SIZE]"
+        echo ". [scriptname].sh --size [SIZE]"
+        echo "Use flag -? with ? to get info about script"
+elif [[ $2 = '?' ]]
+    then
         echo "this script implements a replacement for all files in the directory with a size less than the specified postfix by the first character of the file name."
         echo "[size] -max size of files in current directory to to consider them suitable"
 else
-   for file in $(find -maxdepth 1 -type f -size -$1c)
+   for file in $(find -maxdepth 1 -type f -size -$2c)
             do
              file_name=$(echo $file | rev | cut -d '/' -f1 | rev)
              file_suffix=$(echo $file | rev | cut -d '.' -f1 | rev)
