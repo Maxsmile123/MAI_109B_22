d10795a7dee6ca3748582d7a3b1f055d90b9aee4
Elina Burganova <shlsme@yandex.ru>
2023-03-06 00:30:46 +0300

    bash script is added

---
+++ Burganova/lab21/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+fileName=$1
+suffix=$2
+entranceCheck_1=0
+baseName="${fileName%%.*}"
+
+if [[ !(-f "$fileName") ]];
+then
+    echo "File not found"
+    exit
+fi
+
+if [[ $baseName != *"$suffix" ]];
+then
+    echo "File's suffix doesn't match entered suffix"
+    exit
+fi
+
+for file in *;
+do
+    if [[ -f "$file" ]] && [[ "$file" != "$fileName" ]];
+    then 
+        baseName="${file%%.*}"
+        if [[ $baseName == *"$suffix" ]] && cmp -s "$fileName" "$file";
+        then         
+            rm "$file"
+            entranceCheck_1=1
+        fi
+    fi
+done
+     
+if [[ $entranceCheck_1 == 0 ]];
+then
+    echo "No matches found"
+fi
