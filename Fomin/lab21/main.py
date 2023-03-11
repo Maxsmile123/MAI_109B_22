f2779a4aa803695415c3829c6a0e1b22654b7ce7
Ivan <grenka388@gmail.com>
2023-03-11 11:41:44 +0300

    changes lab21

--- Fomin/lab21/main.py
+++ Fomin/lab21/main.py
@@ -8,27 +8,26 @@ import argparse
 def get_arguments():
     """Returns prefix and suffix (both can be set using regexp)"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--prefix", type=str, help="new file prefix", default="")
+    parser.add_argument("-p", "--prefix", type=str, help="new file prefix", required=True)
     parser.add_argument("-s", "--suffix", type=str, help="current filename ending", default="")
     parser.add_argument("-root", "--root", type=str, help="root directory", default="./")
-    parser.add_argument("-i", "--insensitive", action="store_true")
-    parser.set_defaults(i=False)
+    parser.add_argument("-i", "--insensitive", action="store_true", default=False)
 
     args = parser.parse_args()
 
     return args.prefix, args.suffix, args.insensitive, args.root
 
 
-def get_files(prefix, suffix, case_insensitive, root):
+def get_and_rename_files(prefix, suffix, case_insensitive, root):
     """Gets all the files and through changes their names"""
     for rootDir, dirs, files in os.walk(root):
         for file in files:
             if re.match(rf".*({suffix})(\..+)*", file, re.IGNORECASE if case_insensitive else 0):
-                os.rename(rootDir + "/" + file, rootDir + "/" + prefix + file)
+                os.rename(os.path.join(rootDir, file), os.path.join(rootDir, prefix + file))
 
 
 def main():
-    get_files(*get_arguments())
+    get_and_rename_files(*get_arguments())
 
 
 if __name__ == "__main__":
