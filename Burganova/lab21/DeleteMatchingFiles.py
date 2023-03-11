64d0f275bfdd80bb74d9270e9129cc3978580bc0
Ellinois4 <125216036+Ellinois4@users.noreply.github.com>
2023-03-11 14:50:38 +0300

    Update DeleteMatchingFiles.py

--- Burganova/lab21/DeleteMatchingFiles.py
+++ Burganova/lab21/DeleteMatchingFiles.py
@@ -3,10 +3,14 @@ import filecmp
 import os
 
 
-def getArgs():
-    parser = argparse.ArgumentParser()  
-    parser.add_argument("-f", "--inputFileName", type=str, help='input file name', required=True)
-    parser.add_argument("-suf", "--suffix", type=str, help='End of file name', required=True)
+def readArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f", "--inputFileName", type=str, help="input file name", required=True
+    )
+    parser.add_argument(
+        "-suf", "--suffix", type=str, help="End of file name", required=True
+    )
 
     args = parser.parse_args()
 
@@ -15,37 +19,48 @@ def getArgs():
 
 def checkSuffix(suffix, inputFileName):
     suffixLength = len(suffix)
-    baseFileName = inputFileName.split('.')[0]
+    baseFileName = inputFileName.split(".")[0]
     beginOfCut = len(baseFileName) - suffixLength
     endOfCut = len(baseFileName)
 
-    if suffix == baseFileName[beginOfCut:endOfCut]:
-        return True
-
-    return False
+    return suffix == baseFileName[beginOfCut:endOfCut]
 
 
 def checkFileExsist(inputFileName):
     return os.path.isfile(inputFileName)
 
 
-def deleteMatchingFiles(suffix, inputFileName):
-    lisOfFiles = os.listdir()
+def checkReadPermission(inputFileName):
+    return os.access(inputFileName, os.R_OK)
 
-    for file in lisOfFiles:
-        if checkFileExsist(file) and file != inputFileName:
-            if checkSuffix(suffix, inputFileName) and filecmp.cmp(file, inputFileName, shallow=True):
+
+def deleteMatchingFiles(suffix, inputFileName):
+    fileList = os.listdir()
+
+    for file in fileList:
+        try:
+            if (
+                checkFileExsist(file)
+                and file != inputFileName
+                and checkSuffix(suffix, inputFileName)
+                and filecmp.cmp(file, inputFileName, shallow=True)
+            ):
                 os.remove(file)
+        except PermissionError as err:
+            print(err)
 
 
 def main():
-    suffix, inputFileName = getArgs()
+    suffix, inputFileName = readArgs()
 
     if not checkFileExsist(inputFileName):
-        return print("File not found!")
+        raise FileNotFoundError(f'File {inputFileName} not found!')
 
+    if not checkReadPermission(inputFileName):
+        raise PermissionError(f'Permission denied for input file: {inputFileName}')
+       
     if not checkSuffix(suffix, inputFileName):
-        return print("File's suffix doesn't match entered suffix")
+        raise Exception("File's suffix doesn't match entered suffix!")
 
     deleteMatchingFiles(suffix, inputFileName)
 
